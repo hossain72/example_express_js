917771c72a8b751987595a9663ea3009b2718ae9
Anowar <hossainanowar72@gmail.com>
2022-03-09 15:05:44 +0600

    learn put method

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -5,7 +5,20 @@ exports.createPost = async (request, respose) => {
   respose.status(201).json(newPost);
 };
 
-exports.getAllPost = async (req, res)=>{
-    const allPost = await Post.find();
-    res.status(200).json(allPost);
-}
+exports.getAllPost = async (req, res) => {
+  const allPost = await Post.find();
+  res.status(200).json(allPost);
+};
+
+exports.getSinglePost = async (req, res) => {
+  const singlePost = await Post.findById(req.params.id);
+  res.status(200).json(singlePost);
+};
+
+exports.updatePost = async (req, res) => {
+  const singlePostUpdate = await Post.findByIdAndUpdate(
+    req.params.id,
+    req.body
+  );
+  res.status(200).json(singlePostUpdate);
+};
--- routes/post.route.js
+++ routes/post.route.js
@@ -5,6 +5,10 @@ const postController = require("../controller/post.controller");
 
 router.post("/", postController.createPost);
 
-router.get("/",postController.getAllPost);
+router.get("/", postController.getAllPost);
+
+router.get("/:id", postController.getSinglePost);
+
+router.put("/:id", postController.updatePost);
 
 module.exports = router;
